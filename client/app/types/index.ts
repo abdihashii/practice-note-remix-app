94b238ace61f1a7e32727ef8b4d24778833023c3
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-13 13:36:37 -0500

    Add Note and DTO interfaces for CRUD operations

--- client/app/types/index.ts
+++ client/app/types/index.ts
@@ -1,5 +1,20 @@
 export interface Note {
-  id: string;
+  id: string; // UUID
   title: string;
   content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  favorite: boolean;
+}
+
+export interface CreateNoteDto {
+  title: string;
+  content: string;
+  favorite?: boolean;
+}
+
+export interface UpdateNoteDto {
+  title?: string;
+  content?: string;
+  favorite?: boolean;
 }
