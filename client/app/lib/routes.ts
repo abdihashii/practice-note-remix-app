3ff7de4f0092b0f5ec302a1ad3001e13dc74be64
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-13 16:54:55 -0500

    add api routes file in client

---
+++ client/app/lib/routes.ts
@@ -0,0 +1,28 @@
+import { Note } from "~/types";
+
+export const getNotes = async (API_URL: string): Promise<Note[] | null> => {
+  try {
+    const response = await fetch(`${API_URL}/notes`);
+    const data = await response.json();
+    return data as Note[];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+// export const deleteNode = async (
+//   API_URL: string,
+//   id: string
+// ): Promise<Note | null> => {
+//   try {
+//     const response = await fetch(`${API_URL}/notes/${id}`, {
+//       method: "DELETE",
+//     });
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// };
