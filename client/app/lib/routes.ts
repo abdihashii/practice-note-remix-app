d89f850ca4d148348c4951914c6f5c70ae1d74c1
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-13 18:40:58 -0500

    bring back deleteNote api call

--- client/app/lib/routes.ts
+++ client/app/lib/routes.ts
@@ -11,18 +11,18 @@ export const getNotes = async (API_URL: string): Promise<Note[] | null> => {
   }
 };
 
-// export const deleteNode = async (
-//   API_URL: string,
-//   id: string
-// ): Promise<Note | null> => {
-//   try {
-//     const response = await fetch(`${API_URL}/notes/${id}`, {
-//       method: "DELETE",
-//     });
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error(error);
-//     return null;
-//   }
-// };
+export const deleteNote = async (
+  API_URL: string,
+  id: string
+): Promise<Note | null> => {
+  try {
+    const response = await fetch(`${API_URL}/notes/${id}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
