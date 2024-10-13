ddbb7abf760d21afc9015a7a9a64365b9cc2531d
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-13 18:44:25 -0500

    just added a basic theme switcher

--- client/app/components/Layout/Header.tsx
+++ client/app/components/Layout/Header.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
+import { MoonIcon, SunIcon } from "lucide-react";
+
+import { Button } from "~/components/ui/button";
+
 const Header = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <header className="h-16 flex items-center justify-between px-8 bg-white shadow">
       <h1 className="text-2xl font-bold">My Notes</h1>
-      <nav className="flex space-x-4"></nav>
+
+      <div>
+        <Button variant="outline" size="icon" onClick={toggleTheme}>
+          {theme === "light" ? (
+            <MoonIcon className="w-4 h-4" />
+          ) : (
+            <SunIcon className="w-4 h-4" />
+          )}
+        </Button>
+      </div>
     </header>
   );
 };
