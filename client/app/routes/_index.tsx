986966bc7de899f6d6fdd3a30218df1ca58f9288
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-13 18:40:15 -0500

    remove server-side call stuff

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,11 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useLoaderData, json } from "@remix-run/react";
 
-import { getNotes } from "~/lib/routes";
-
+import FloatingActionButton from "~/components/FloatingActionButton";
 import Layout from "~/components/Layout/Layout";
 import NoteCard from "~/components/notes/NoteCard";
-import FloatingActionButton from "~/components/FloatingActionButton";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,30 +11,11 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function loader() {
-  const API_URL = process.env.API_URL;
-
-  if (!API_URL) {
-    console.error("API_URL is not set");
-    return { notes: [] };
-  }
-
-  const notes = await getNotes(API_URL);
-
-  if (!notes) {
-    throw new Response("No notes found", { status: 404 });
-  }
-
-  return json({ notes });
-}
-
 export default function Index() {
-  const { notes } = useLoaderData<typeof loader>();
-
   return (
     <Layout>
       <div className="relative flex-grow">
-        {notes.length > 0 ? (
+        {/* {notes.length > 0 ? (
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
             {notes.map((note) =>
               note ? (
@@ -51,7 +29,7 @@ export default function Index() {
           </div>
         ) : (
           <p className="text-center text-gray-600">No notes yet. Create one!</p>
-        )}
+        )} */}
         <FloatingActionButton />
       </div>
     </Layout>
