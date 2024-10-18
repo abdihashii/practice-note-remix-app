8bd5b624faaa4e1f09798f4699f11e49f7bed4df
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-17 17:06:56 -0700

    better search component

--- client/app/components/common/SearchBar.tsx
+++ client/app/components/common/SearchBar.tsx
@@ -3,8 +3,6 @@ import { Form, useSubmit } from "@remix-run/react";
 import { useState } from "react";
 
 // Third party components
-import { SearchIcon } from "lucide-react";
-import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 
 const SearchBar = () => {
@@ -21,18 +19,16 @@ const SearchBar = () => {
     <Form
       method="get"
       onSubmit={handleSubmit}
-      className="flex w-full max-w-sm items-center space-x-2"
+      className="flex w-full items-center gap-2 h-12"
     >
       <Input
         type="search"
         name="q"
-        placeholder="Search notes..."
+        placeholder="Type / to search notes"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
+        className="h-full"
       />
-      <Button type="submit" size="icon">
-        <SearchIcon className="h-4 w-4" />
-      </Button>
     </Form>
   );
 };
