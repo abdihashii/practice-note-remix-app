2d861b01b4cf37b57b386ad6b1c1f47b7596df18
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-21 22:34:21 -0500

    Create a protected route layout component

---
+++ client/app/components/common/layout/ProtectedLayout.tsx
@@ -0,0 +1,18 @@
+import Header from "~/components/common/Header";
+
+interface ProtectedLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex flex-col">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full py-6 flex flex-col gap-4">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+}
