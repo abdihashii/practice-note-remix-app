0b10cea45861eef896f3659d2c34acae19895970
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2024-10-26 14:12:18 -0500

    mov out the dark mode togle to its own component

--- client/app/components/common/layout/Header.tsx
+++ client/app/components/common/layout/Header.tsx
@@ -1,20 +1,10 @@
-// Third party libraries
-import { Theme, useTheme } from "remix-themes";
-
-// Third party components
-import { MoonIcon, SunIcon } from "lucide-react";
-
 // First party components
-import { Button } from "~/components/ui/button";
 import { Link } from "@remix-run/react";
 
+// First party components
+import DarkModeToggleButton from "~/components/common/layout/DarkModeToggleButton";
+
 const Header = () => {
-  const [theme, setTheme] = useTheme();
-
-  const toggleTheme = () => {
-    setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
-  };
-
   return (
     <header className="sticky top-0 z-10 bg-background/80 backdrop-blur-sm border-b border-foreground/10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -26,23 +16,7 @@ const Header = () => {
             My Notes
           </Link>
 
-          <div className="flex items-center space-x-2 sm:space-x-4">
-            <Button
-              variant="ghost"
-              size="icon"
-              onClick={toggleTheme}
-              className="h-8 w-8 sm:h-9 sm:w-9 rounded-full hover:bg-foreground/10"
-              aria-label={`Switch to ${
-                theme === "light" ? "dark" : "light"
-              } mode`}
-            >
-              {theme === "light" ? (
-                <MoonIcon className="h-4 w-4 sm:h-5 sm:w-5" />
-              ) : (
-                <SunIcon className="h-4 w-4 sm:h-5 sm:w-5" />
-              )}
-            </Button>
-          </div>
+          <DarkModeToggleButton />
         </div>
       </div>
     </header>
