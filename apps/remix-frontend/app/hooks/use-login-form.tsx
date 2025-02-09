60ea88cdc85f81d0a768d9aca3174415741fda63
abdihashii <9002684+abdihashii@users.noreply.github.com>
2025-02-09 14:57:28 -0600

    feat: add useLoginForm hook for login form state management

---
+++ apps/remix-frontend/app/hooks/use-login-form.tsx
@@ -0,0 +1,27 @@
+import { useAuth } from "./use-auth";
+
+export default function useLoginForm() {
+  const { loginMutation, loginError, isAuthenticated, isAuthQueryPending } =
+    useAuth();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    loginMutation.mutate({
+      email,
+      password,
+    });
+  };
+
+  return {
+    loginMutation,
+    loginError,
+    isAuthenticated,
+    isAuthQueryPending,
+    handleSubmit,
+  };
+}
